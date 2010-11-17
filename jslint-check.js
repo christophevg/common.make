1904f3230e93d9f7295bbe647e7c9de8825a66ce
Christophe VG <xtof@pumpkin.local>
2010-11-17 18:52:19 +0100

    added evidence that is ok and shouldn't be reported

--- jslint-check.js
+++ jslint-check.js
@@ -9,7 +9,7 @@ JSLINT( src, { evil: true, forin: true } );
 // (in contradiction with JSLint) more information here:
 // http://docs.jquery.com/JQuery_Core_Style_Guidelines
 // and (soon) at http://umlcanvas.org/Development/Coding_Guidelines
-var ok = {
+var ok_reason = {
   "Use '===' to compare with 'null'." : true,
   "Use '!==' to compare with 'null'." : true,
   "Expected a 'break' statement before 'case'." : true,
@@ -17,13 +17,17 @@ var ok = {
   "Reserved name '__proto__'." : true
 };
 
+var ok_evidence = {
+  "  fnTest = /xyz/.test(function(){xyz;}) ? /\\b_super\\b/ : /.*/;" : true
+};
+
 var e = JSLINT.errors, found = 0, w;
 
 for( var i = 0; i < e.length; i++ ) {
   w = e[i];
 
-  if( !ok[ w.reason ] ) {
+  if( !ok_reason[ w.reason ] && !ok_evidence[ w.evidence ] ) {
     print( "JSLint> " + fileName + " @ " + w.line + "," + w.character + ": " + 
-    w.reason + "\n   " + w.evidence + "\n" );
+    w.reason + "\n  " + w.evidence + "\n" );
   }
 }
